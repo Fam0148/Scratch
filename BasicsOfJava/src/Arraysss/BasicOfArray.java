c8f130521bf5a5dc894d9fc577973fc6862050d1
Fam0148 <subashssubramaniank@gmail.com>
2022-04-02 13:06:28 +0530

    Array_Basics

---
+++ BasicsOfJava/src/Arraysss/BasicOfArray.java
@@ -0,0 +1,21 @@
+package Arraysss;
+
+import java.util.Arrays;
+
+public class BasicOfArray 
+{
+	public static void main(String[] args) 
+	{
+		int marks[] = new int[4]; // Default it Fills Zero.
+		String name[] = new String[4]; // Default it Fills Null.
+		boolean isTrue[] = new boolean[4];// Default it Fills False.
+//----------------------------------------------------------------------------		
+		int marks1[] = {1,2,3,4};
+		System.out.println(Arrays.toString(marks1));
+		System.out.println(Arrays.toString(marks));
+		System.out.println(Arrays.toString(name));
+		System.out.println(Arrays.toString(isTrue));
+//----------------------------------------------------------------------------
+		System.out.print(Arrays.equals(marks1,marks1)); // Compare To Array.
+	}
+}
