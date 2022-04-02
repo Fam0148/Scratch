fd338c92af3507efe52eb5208a5a3d17491019f1
Fam0148 <subashssubramaniank@gmail.com>
2022-04-02 20:19:26 +0530

    ArrayList

---
+++ BasicsOfJava/src/Arraysss/List.java
@@ -0,0 +1,20 @@
+package Arraysss;
+
+import java.util.ArrayList;
+
+public class List 
+{
+public static void main(String[] args) 
+{	
+	// ArrayList is to OverCome the Array:
+	
+	ArrayList name = new ArrayList();
+	name.add("SUBASH");
+	name.add("SIVA");
+	System.out.println(name);
+	name.remove(0);
+	System.out.println(name);
+	
+	// In ArrarList we can make add and Remove Very Easy.
+}
+}
