a3001be671ecb93189fa073ecc0ebe176ae5707d
Fam0148 <subashssubramaniank@gmail.com>
2022-04-02 13:07:51 +0530

    Array_Basics

--- BasicsOfJava/src/Arraysss/BasicOfArray.java
+++ BasicsOfJava/src/Arraysss/BasicOfArray.java
@@ -7,12 +7,14 @@ public class BasicOfArray
 	public static void main(String[] args) 
 	{
 		int marks[] = new int[4]; // Default it Fills Zero.
+		double decimal[] = new double[4];// Default it Fills decimal digits.
 		String name[] = new String[4]; // Default it Fills Null.
 		boolean isTrue[] = new boolean[4];// Default it Fills False.
 //----------------------------------------------------------------------------		
 		int marks1[] = {1,2,3,4};
 		System.out.println(Arrays.toString(marks1));
 		System.out.println(Arrays.toString(marks));
+		System.out.println(Arrays.toString(decimal));
 		System.out.println(Arrays.toString(name));
 		System.out.println(Arrays.toString(isTrue));
 //----------------------------------------------------------------------------
